0068d0a04694f38561b4e60f7b1a0e8136c80db9
Piyush Chauhan <piyushchauhan0490@gmail.com>
2025-12-19 01:11:40 +0530

    array functions

--- 4_Arrays/2_array.js
+++ 4_Arrays/2_array.js
@@ -14,3 +14,19 @@ console.log("Concatenating hero3 in hero2: ", allHeroes);
 //using spread operator ...
 const combinedHeroes = [...hero2, ...hero3, ...dc_heroes]; // Merges hero2, hero3, and dc_heroes into a new array
 console.log("Combining hero2, hero3 and dc_heroes: ", combinedHeroes);
+
+//using flat()
+const myHeroes = ["Marvel Heroes", ["Spiderman", "Ironman", ["Hulk", "Thor"]], "DC Heroes", ["Batman", "Superman", ["Flash", ["Green Lantern", "Aquaman"]]]];
+console.log("My Heroes Array: ", myHeroes);
+console.log("Flattening myHeroes: ", myHeroes.flat(3));
+
+//is, of, from in arrays
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+const scoresArray = Array.of(score1, score2, score3); // Creates an array from the given scores
+console.log("Scores Array using Array.of(): ", scoresArray);
+
+const scoresFromArray = Array.from("Piyush"); // Creates an array from the string "400"
+console.log("Scores Array using Array.from(): ", scoresFromArray);
